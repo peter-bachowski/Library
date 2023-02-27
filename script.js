6bf79b03e43fed57394b5a1b91bdb80dfb0e426e
Peter <bachowskip@gmail.com>
2023-02-26 23:13:47 -0800

    Implement the algorithm for removing books, and fix issue with book ids not matching

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const submitBtn = document.getElementById("submitBtn");
 let myLibrary = [];
 let title = document.getElementById("bookTitle");
 let author = document.getElementById("bookAuthor");
-let pages = document.getElementById("bookPages")
+let pages = document.getElementById("bookPages");
+let bookCount = 0;
 
 //constructor
 function Book(title, author, pages) {
@@ -32,17 +33,46 @@ submitBtn.onclick = (event) => {
 //submitBtn.addEventListener("click", addBookToLibrary);
 
 function addBookToLibrary(title, author, pages, event) {
-  event.preventDefault();
-  const newBook = new Book(title, author, pages);
-  myLibrary.push(newBook);
+
+  event.preventDefault(); //removes the check if input filled requirement for the form
+  bookCount += 1;
+  const bookObject = new Book(title, author, pages);
+  myLibrary.push(bookObject);
+
+  const book = document.createElement("div");
+  const removeBookBtn = document.createElement("button");
+
   Book.prototype.toString = function bookTitleToString() {
-    return `${this.title}`;
+    return this.title + "\n\n" + this.author + "\n\n" + this.pages + " pages";
   };
-  let newBookBtn = document.createElement("div");
-  let newBookBtnClose = document.createElement("button");
-  newBookBtn.appendChild(newBookBtnClose);
-  newBookBtnClose.id = "newBookBtnClose";
-  newBookBtn.id = "book" + myLibrary.length;
-  newBookBtn.innerText = newBook.toString();
-  bookContainer.appendChild(newBookBtn);
+
+  book.classList.add("book");
+  removeBookBtn.classList.add("removeBookBtn");
+  removeBookBtn.innerText = "Delete";
+
+  book.id = "book" + bookCount;
+  removeBookBtn.id = "remove" + book.id;
+
+  removeBookBtn.onclick = () => {
+    removeFromMyLibrary(book);
+    bookContainer.removeChild(book);
+  }
+
+  book.innerText = bookObject.toString();
+
+  bookContainer.appendChild(book);
+  book.appendChild(removeBookBtn);
+}
+
+function removeFromMyLibrary (book) {
+  const bookElements = document.querySelectorAll(".book");
+  for (let i = 0; i < myLibrary.length; i++){
+    if(bookElements[i].id === book.id){
+      for (let j = i; j < myLibrary.length; j++){
+        myLibrary[j] = myLibrary[j + 1];
+      }
+      myLibrary.length = myLibrary.length - 1;
+      break;
+    }
+  }
 }
